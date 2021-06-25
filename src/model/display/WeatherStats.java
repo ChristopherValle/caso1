50ecdd91c5842ed1f4d5c78a599410740c59179f
CHRISTOPHER VALLE PERAZA <christopher.valle.peraza@est.una.ac.cr>
2021-06-24 18:31:59 -0600

    moved values to folder

--- src/model/display/WeatherStats.java
+++ src/model/display/WeatherStats.java
@@ -6,7 +6,7 @@ public class WeatherStats extends WeatherDisplay{
     public void displayInfo(){
 
         System.out.println("Con una temperatura de " + super.values.getTemp() + " grados y una humedad de " + super.values.getHumid());
-        System.out.println("los siguientes dias podrian tener una temperatura de:" + futureTemp() );
+        System.out.println("los siguientes dias podrian tener una temperatura de:" + futureTemp() + "\n" );
 
     }
     public double futureTemp(){
